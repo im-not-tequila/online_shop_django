1a21234246ccb89d88effc1636719b50b2760758
im-not-tequila <i.am.tequila1236@gmail.com>
2022-11-02 17:47:48 +0600

    2022 11 02

--- product/views.py
+++ product/views.py
@@ -1,10 +1,12 @@
+from django.db.models import Q
 from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
 
 class LatestProductsList(APIView):
@@ -18,8 +20,6 @@ class LatestProductsList(APIView):
 class ProductDetail(APIView):
     def get_object(self, category_slug, product_slug):
         try:
-            print(category_slug)
-            print(product_slug)
             return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
         except Product.DoesNotExist:
             raise Http404
@@ -29,3 +29,31 @@ class ProductDetail(APIView):
         serializer = ProductSerializer(product)
 
         return Response(serializer.data)
+
+
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, format=None):
+        print(category_slug)
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
+
+        return Response(serializer.data)
+
+
+@api_view(['POST'])
+def search(request):
+    query = request.data.get('query', '')
+
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+    else:
+        return Response({"products": []})
