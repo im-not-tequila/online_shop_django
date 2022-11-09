79eb3ffede02059423df9f18e553983ef23fdccc
im-not-tequila <i.am.tequila1236@gmail.com>
2022-11-09 11:14:58 +0600

    2022 11 09

--- product/models.py
+++ product/models.py
@@ -1,6 +1,8 @@
 from io import BytesIO
 from PIL import Image
 
+import os
+
 from django.core.files import File
 from django.db import models
 
@@ -40,18 +42,18 @@ class Product(models.Model):
 
     def get_image(self):
         if self.image:
-            return 'http://127.0.0.1:8000' + self.image.url
+            return os.environ.get("CURRENT_HOST") + self.image.url
         return ''
 
     def get_thumbnail(self):
         if self.thumbnail:
-            return 'http://127.0.0.1:8000' + self.thumbnail.url
+            return os.environ.get("CURRENT_HOST") + self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_thumbnail(self.image)
                 self.save()
 
-                return 'http://127.0.0.1:8000' + self.thumbnail.url
+                return os.environ.get("CURRENT_HOST") + self.thumbnail.url
             else:
                 return ''
 
